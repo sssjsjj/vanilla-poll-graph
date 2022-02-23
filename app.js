6912817227e760805fcf1655ba00f7fa86f5af23
정수진 <sssjsjj@ipartners.co.kr>
2022-02-23 17:18:56 +0900

    build: server, webpack 세팅

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const path = url.parse(req.url, true).pathname; // url에서 path 추출
+  res.statusCode = 200;
+  if (path === '/dist/js/bundle.js') {
+    res.setHeader('Content-Type', 'text/javascript');
+    fs.readFile(__dirname + '/dist/js/bundle.js', (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      res.end(data);
+    });
+  } else if (path === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile(__dirname + '/index.html', (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      res.end(data, 'utf-8');
+    });
+  } else {
+    res.statusCode = 404; // 404 상태 코드
+    res.end('주소가 없습니다');
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
